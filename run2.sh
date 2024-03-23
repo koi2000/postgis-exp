1a8a70b99b4b7af9236157ddfa50a68b696dcbc1
koi2000 <koi20000@163.com>
2024-03-23 12:40:53 +0800

    add log

--- run2.sh
+++ run2.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-for ((i = 0; i <= 9551; i++)); do
-    ./build/within -t $i -1 nuclei -2 nuclei
-done
+# for ((i = 0; i <= 9551; i++)); do
+#     ./build/within -t $i -1 nuclei -2 nuclei
+# done
 
 # for ((i = 0; i <= 9551; i++)); do
 #     ./build/nn -t $i -1 nuclei -2 vessel
 # done
 
-# for ((i = 0; i <= 9551; i++)); do
-#     ./build/within_origin -t $i -1 nuclei -2 nuclei
-# done
+for ((i = 0; i <= 9551; i++)); do
+    ./build/within_origin -t $i -1 nuclei -2 nuclei
+done
 
 # for ((i = 0; i <= 9551; i++)); do
 #     ./build/nn_origin -t $i -1 nuclei -2 vessel
