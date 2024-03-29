e821e90d4e599dc993f5347bc5c9824636540317
koi2000 <koi20000@163.com>
2024-03-29 22:05:22 +0800

    add nn all vessel log

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-nohup ./build/main --num 9551 --table2 vessel --queryType nn --queryMethod progressive --samplerate 1 > res.log
+nohup ./build/main --num 9551 --table2 vessel --queryType nn --queryMethod origin --samplerate 10 > res.log
